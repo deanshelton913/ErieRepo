cfee602fe6ea0688477829d2d3638ae3eb56e8ce
Dean Shelton <dshelton@sbgtv.com>
2021-03-10 20:56:31 -0800

    Adds some tv magic.

---
+++ terraform/main.tf
@@ -0,0 +1,59 @@
+provider "aws" {
+  version = "~> 2.0"
+  region  = "us-west-2"
+}
+
+resource "aws_ecr_repository" "flask" {
+  name = "flask-repo"
+}
+
+resource "aws_ecs_cluster" "my_cluster" {
+  name = "flaskmagic"
+}
+
+resource "aws_ecs_task_definition" "my_first_task" {
+  family                   = "my-first-task" # Naming our first task
+  container_definitions    = <<DEFINITION
+  [
+    {
+      "name": "my-first-task",
+      "image": "${aws_ecr_repository.flask.repository_url}",
+      "essential": true,
+      "portMappings": [
+        {
+          "containerPort": 8080,
+          "hostPort": 8080
+        }
+      ],
+      "memory": 512,
+      "cpu": 256
+    }
+  ]
+  DEFINITION
+  requires_compatibilities = ["FARGATE"] # Stating that we are using ECS Fargate
+  network_mode             = "awsvpc"    # Using awsvpc as our network mode as this is required for Fargate
+  memory                   = 512         # Specifying the memory our container requires
+  cpu                      = 256         # Specifying the CPU our container requires
+  execution_role_arn       = "${aws_iam_role.ecsTaskExecutionRole.arn}"
+}
+
+resource "aws_iam_role" "ecsTaskExecutionRole" {
+  name               = "ecsTaskExecutionRole"
+  assume_role_policy = "${data.aws_iam_policy_document.assume_role_policy.json}"
+}
+
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
+  role       = "${aws_iam_role.ecsTaskExecutionRole.name}"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
